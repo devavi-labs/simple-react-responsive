043f00dcc0952edd956407702d89fb5c02c90539
Avinash Sinha <savinash2608@gmail.com>
2020-09-27 09:25:01 +0530

    version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# simple-react-responsive
+# Simple React Responsive
+
+A simple React component for media query breakpoints.
+
+## Install
+
+### NPM
+
+```
+$ npm i simple-react-responsive
+```
+
+### Yarn
+
+```
+$ yarn add simple-react-responsive
+```
+
+## Usage
+
+After installing, Wrap the root component in <ResponsiveBreakpointsProvider>. This will provide the default breakpoints inside your app.
+
+### Example
+
+```
+import React from 'react'
+import { ResponsiveBreakpointsProvider } from 'simple-react-responsive'
+
+const App = () => {
+    return (
+        <ResponsiveBreakpointsProvider>
+        // ...all your other components here
+        </ResponsiveBreakpointsProvider>
+    )
+}
+
+export default App
+```
+
+Then use <Responsive> component to wrap any component to make it responsive.
+
+### Example 
+
+The following code will render the <MyComponent> only in devices with screen size larger than mobile devices.
+
+```
+<Responsive
+    type="above"
+    screen="mobile"
+>
+    <MyComponent>
+</Responsive>
+```
+
+## Custom Breakpoints
+
+You can define your own breakpoints when you use the <ResponsiveBreakpointsProvider> like
+
+```
+const customBreakpoints = {
+    mobile: 0,
+    tablet: 768,
+    desktop: 992,
+    largeScreen: 1200,
+    wideScreen: 1920
+}
+
+return (
+    <ResponsiveBreakpointsProvider breakpoints={customBreakpoints}>
+    // ...all your other components here
+    </ResponsiveBreakpointsProvider>
+)
+```
+
+## Props
+
+You can pass the following props in the <Responsive> component
+
+| Name    | Expected values                                            | Default   |
+| ------- | ---------------------------------------------------------- | --------- |
+| type    | "below", "only" or "above"                                 | "above"   |
+| screen  | "mobile", "tablet", "desktop", "largeScreen", "wideScreen" | "mobile"  |
---
+++ src/index.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react'
+import MediaQuery from 'react-responsive'
+
+export interface IResponsiveBreakpoints {
+    // Minimum width for mobile devices
+    mobile?: number | undefined
+    // Minimum width for tablet devices
+    tablet?: number | undefined
+    // Minimum width for desktop devices
+    desktop?: number | undefined
+    // Minimum width for large screen devices
+    largeScreen?: number | undefined
+    // Minimum width for wide screen devices
+    wideScreen?: number | undefined
+}
+
+export const defaultResponsiveBreakpoints: IResponsiveBreakpoints = {
+    mobile: 0,
+    tablet: 768,
+    desktop: 992,
+    largeScreen: 1200,
+    wideScreen: 1920
+}
+
+export const BreakpointsContext = React.createContext(defaultResponsiveBreakpoints)
+
+export interface IResponsiveBreakpointsProviderProps {
+    breakpoints?: IResponsiveBreakpoints
+}
+
+export const ResponsiveBreakpointsProvider: React.FC<IResponsiveBreakpointsProviderProps> = (
+    { breakpoints = defaultResponsiveBreakpoints, children }
+) => {
+    return (
+        <BreakpointsContext.Provider value={{ ...defaultResponsiveBreakpoints, ...breakpoints }}>
+            {children}
+        </BreakpointsContext.Provider>
+    )
+}
+
+export type IResponsiveScreens = "mobile" | "tablet" | "desktop" | "largeScreen" | "wideScreen"
+
+export interface IResponsiveProps {
+    /**
+     * Type of media query
+     * @example type="below" ==> "(max-width: any)"
+     * @example type="only" ==> "(min-width: any) and (max-width: any)"
+     * @example type="above" ==> "(min-width: any)"
+     */
+    type?: "below" | "only" | "above"
+    screen?: IResponsiveScreens
+}
+
+export const Responsive: React.FC<IResponsiveProps> = (
+    { type = "above", screen = "mobile", children }
+) => {
+    const breakpoints = React.useContext(BreakpointsContext)
+    let min: number | undefined
+    let max: number | undefined
+    let upperLevelScreen: IResponsiveScreens | undefined
+
+    upperLevelScreen = screen === "mobile" ? "tablet"
+        : screen === "tablet" ? "desktop"
+            : screen === "desktop" ? "largeScreen"
+                : "wideScreen"
+    min = type === "only" ? breakpoints[screen]
+        : type === "above" ? breakpoints[upperLevelScreen]! - 1
+            : undefined
+    max = type === "below" ? breakpoints[screen]
+        : type === "only" ? breakpoints[upperLevelScreen]! - 1
+            : undefined
+
+    console.log(breakpoints)
+
+    return (
+        <MediaQuery minWidth={min} maxWidth={max} >
+            {children}
+        </MediaQuery>
+    )
+}
+
+export default Responsive
+
